b895badc534641e145d199661df901e84ead05dd
Prabhat <singh.prabhat8991@gmail.com>
2022-09-01 22:41:51 +0530

    recursion quicksort

---
+++ src/recursion/QuickSort.kt
@@ -0,0 +1,52 @@
+package recursion
+
+fun main() {
+  var input = intArrayOf(5,1,1,2,0,0)
+    quickSort(input, 0, 5)
+    input.forEach {
+        print(it)
+    }
+}
+
+fun quickSort(arr: IntArray, start: Int, end: Int) {
+    if (start >= end) {
+        return
+    }
+
+    val index = partition(arr, start, end)
+    quickSort(arr, start, index-1)
+    quickSort(arr, index + 1, end)
+}
+
+fun partition(arr: IntArray, start: Int, end: Int): Int {
+    val pivot = arr[start]
+    var cnt = 0
+    for (i in start+1..end) {
+        if (arr[i] <= pivot) {
+            cnt++
+        }
+    }
+    var pivotIndex = start + cnt
+    var temp = arr[pivotIndex]
+    arr[pivotIndex] = pivot
+    arr[start] = temp
+
+    var counterLeft = start
+    var counterRight = end
+    while (counterLeft < pivotIndex && counterRight > pivotIndex) {
+        while (arr[counterLeft] <= pivot) {
+            counterLeft++
+        }
+        while (arr[counterRight] >  pivot) {
+            counterRight--
+        }
+        if (counterLeft < pivotIndex && counterRight > pivotIndex) {
+            val temporary = arr[counterRight]
+            arr[counterRight] = arr[counterLeft]
+            arr[counterLeft] = temporary
+            counterLeft++
+            counterRight--
+        }
+    }
+    return pivotIndex
+}
---
+++ src/recursion/Subsets.kt
@@ -0,0 +1,25 @@
+package recursion
+
+fun main() {
+  subsets(intArrayOf(1,2,3)).forEach {
+      print(it)
+  }
+}
+
+fun subsets(nums: IntArray): List<List<Int>> {
+    val output = mutableListOf<Int>()
+    val ans = mutableListOf<List<Int>>()
+    solve(nums, output, 0, ans)
+    return ans
+}
+
+fun solve(nums: IntArray, output: MutableList<Int>, index: Int, ans: MutableList<List<Int>>) {
+    if (index >= nums.size) {
+        ans.add(output)
+        return
+    }
+    solve(nums, output, index + 1, ans)
+    val element = nums[index]
+    output.add(element)
+    solve(nums, output, index + 1, ans)
+}
