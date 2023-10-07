b100091126754359b1e2948617b0bec50fa18d37
Prabhat8991 <singh.prabhat8991@gmail.com>
2023-10-07 11:04:54 +0530

    tower of hanoi with count

---
+++ src/phase2/recursion/TowerOfHanoiWithCount.java
@@ -0,0 +1,20 @@
+package phase2.recursion;
+
+public class TowerOfHanoiWithCount {
+    public static void main(String[] args) {
+        System.out.println(toh(3, 1, 3, 2));
+    }
+
+    public static long toh(int N, int from, int to, int aux) {
+        long sum = 0;
+        if (N == 1) {
+            System.out.println("move disk 1 from " + from + " to " + to);
+            return 1;
+        }
+        sum = sum + toh(N-1, from, aux, to);
+        sum += 1;
+        System.out.println("move disk " + N + " from " + from + " to " + to);
+        sum = sum + toh(N-1, aux, to, from);
+        return sum;
+    }
+}
