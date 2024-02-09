29f510e334bfc681d71ffdee4f79fb5bdad59473
prabhat.singh <prabhat.singh@thoughworks.com>
2024-02-09 16:09:41 +0530

    implemented stack using linked list

--- src/phase2/stack/StackLinkedListImpl.java
+++ src/phase2/stack/StackLinkedListImpl.java
@@ -1,2 +1,46 @@
-package phase2.stack;public class StackLinkedListImpl {
+package phase2.stack;
+
+import phase2.linkedlist.Node;
+
+public class StackLinkedListImpl {
+    private Node head;
+    private int size;
+    private int nodeCount;
+
+    StackLinkedListImpl(int size, int initialValue) {
+        this.size = size;
+        head = new Node(initialValue);
+    }
+
+    public void push(int value) {
+        if (nodeCount == size-1) {
+            System.out.println("Limit reached....");
+            return;
+        }
+
+        Node node = new Node(value);
+        node.next = head;
+        head = node;
+        nodeCount+=1;
+    }
+
+    public int peek() {
+        return head.value;
+    }
+
+    public int pop() {
+
+        if (head.next == null) {
+            System.out.print("Last element in the stack");
+            return -1;
+        }
+        int result = head.value;
+        head = head.next;
+        nodeCount-=1;
+        return result;
+    }
+
+    public int size() {
+        return nodeCount;
+    }
 }
--- src/phase2/stack/StackLinkedListImplPlayground.java
+++ src/phase2/stack/StackLinkedListImplPlayground.java
@@ -1,2 +1,20 @@
-package phase2.stack;public class StackLinkedListImplPlayground {
+package phase2.stack;
+
+public class StackLinkedListImplPlayground {
+
+    public static void main(String[] args) {
+        StackLinkedListImpl stackLinkedList = new StackLinkedListImpl(5, 1);
+        stackLinkedList.push(2);
+        stackLinkedList.push(3);
+        System.out.println(stackLinkedList.peek());
+        stackLinkedList.push(4);
+        System.out.println(stackLinkedList.peek());
+        stackLinkedList.push(5);
+        stackLinkedList.push(6);
+        System.out.println(stackLinkedList.pop());
+        System.out.println(stackLinkedList.peek());
+        System.out.println(stackLinkedList.pop());
+        System.out.println(stackLinkedList.size());
+        System.out.println(stackLinkedList.pop());
+    }
 }
