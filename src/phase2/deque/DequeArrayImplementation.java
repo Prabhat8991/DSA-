9d240e5d5cbf8e1323132236b075bfb4b6d6e7bb
prabhat.singh <prabhat.singh@thoughworks.com>
2024-02-26 21:54:08 +0530

    implemented deque

--- src/phase2/deque/DequeArrayImplementation.java
+++ src/phase2/deque/DequeArrayImplementation.java
@@ -1,2 +1,52 @@
-package phase2.deque;public class DequeArrayImplementation {
+package phase2.deque;
+
+public class DequeArrayImplementation {
+
+    private int [] array;
+
+    private int size = 0;
+
+    private int front = 0;
+
+    private int capacity = 0;
+
+    DequeArrayImplementation(int capacity) {
+        array = new int[capacity];
+        this.capacity = capacity;
+    }
+
+    public void insertFront(int value) {
+        front = (front - 1 + capacity)%capacity;
+        array[front] = value;
+        size++;
+    }
+
+    public void removeFront() {
+        front = (front + 1)%capacity;
+        size--;
+    }
+
+    public void insertRear(int value) {
+        //because array[((front + size -1 + 1)% capacity)] = value;  +1 and -1 cancels out
+        array[((front + size)% capacity)] = value;
+        size++;
+    }
+
+    public void removeRear() {
+        if (size == 0) {
+            return;
+        }
+        size--;
+    }
+
+    public int getFront() {
+        return array[front];
+    }
+
+    public int getRear() {
+        if (size == 0) {
+            return -1;
+        }
+        return array[(front + size -1)% capacity];
+    }
 }
--- src/phase2/deque/DequeImplementationPlayground.java
+++ src/phase2/deque/DequeImplementationPlayground.java
@@ -1,2 +1,19 @@
-package phase2.deque;public class DequeImplementationPlayground {
+package phase2.deque;
+
+public class DequeImplementationPlayground {
+
+    public static void main(String[] args) {
+        DequeArrayImplementation deque = new DequeArrayImplementation(5);
+        deque.insertFront(10);
+        deque.insertRear(50);
+        System.out.println(deque.getFront());
+        System.out.println(deque.getRear());
+        deque.insertFront(20);
+        System.out.println(deque.getFront());
+        System.out.println(deque.getRear());
+
+
+
+
+    }
 }
